231e8c163ef37c28dd177a171ce89478e53e10d2
BaeHyeungYoung <baetrap@naver.com>
2020-01-16 12:54:27 +0900

    개발자 정보 스크립트 추가
    매니저 스크립트 추가

---
+++ Assets/Scripts/Manager/Manager.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// 작성일자 : 2020-01-16-PM-12-52
+// 작성자   : 배형영
+// 간단설명 :
+
+public class Manager : MonoBehaviour
+{
+    // -------------  Variable  ------------- 
+    
+    
+
+    // -------------  Property  ------------- 
+
+
+
+    // -------------  Behaviour  ------------- 
+
+
+
+    // ------------- Private Method  ------------- 
+
+
+
+    // ------------- ProtectedMethod  ------------- 
+
+
+
+    // -------------  Public Method  ------------- 
+
+
+
+}
